68b2d6b8138b0057fc701bc51ffd05dcbb103418
Mohamed El aassal <142220085+MohamedElaassal@users.noreply.github.com>
2025-08-28 15:20:28 +0100

    Add Prometheus metrics to app.js after deploying Prometheus and Grafana using Helm

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const client = require('prom-client');
 const app = express();
+
+// Collect default metrics
+client.collectDefaultMetrics();
 app.get('/', (req, res) => {
 res.send('Hello, Kubernetes CI/CD!');
 });
+
+app.get('/metrics', async (req, res) => {
+res.set('Content-Type', client.register.contentType);
+res.end(await client.register.metrics());
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
